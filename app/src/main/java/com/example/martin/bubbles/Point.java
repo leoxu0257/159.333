7ec190770f8c707aad359c31f038eda3537810fd
robin wu <58405563+robinwu1012@users.noreply.github.com>
2019-12-24 22:14:59 +1300

    Update Point.java

--- app/src/main/java/com/example/martin/bubbles/Point.java
+++ app/src/main/java/com/example/martin/bubbles/Point.java
@@ -1,11 +1,17 @@
 package com.example.martin.bubbles;
 
 public class Point {
+  
 public float x;
-  public float y;
-}
+public float y;
+
 
 public Point(){
   
+}
+
+public Point(float x, float y){
+  this.x = x;
+  this.y = y;
 }
 }
