65bacd60c797abf430759b3638145163f91a0578
ArnoxN1 <43973107+ArnoxN1@users.noreply.github.com>
2019-12-26 23:59:40 +1300

    Add cross determine function.

--- app/src/main/java/com/example/martin/bubbles/BaseBubble.java
+++ app/src/main/java/com/example/martin/bubbles/BaseBubble.java
@@ -10,36 +10,36 @@ import java.util.List;
 // base bubble
 
 public abstract class BaseBubble {
-/**
+    /**
      * vertex
      */
   protected List<Point> points;
   
   protected boolean isMove;
-  /**
+    /**
      * support the painting in the canvas after set the parameters
      */
-   protected Paint paint;
- /**
+  protected Paint paint;
+    /**
      *  The center of horizontal axis of the bubble
      */ 
-     protected float centerX;
-  /**
+  protected float centerX;
+    /**
      *   the center of vertical axis of bubble
      */
   protected float centerY;
   
-   /**
+    /**
      * Determine the object is dragged or not
      */
-    protected boolean isDragDrop;
+   protected boolean isDragDrop;
   
-    protected float polygonR;
+   protected float polygonR;
    protected float mDx = 16;
   
-    protected float mDy = 16;
+   protected float mDy = 16;
   
-  /**
+    /**
      * View's width and height
      */
   protected int mWidth;
@@ -47,7 +47,7 @@ public abstract class BaseBubble {
   protected int mHeight;
   
   
-     public BaseBubble(float drawX, float drawY, float polygonR, int width, int height) {
+  public BaseBubble(float drawX, float drawY, float polygonR, int width, int height) {
         this.centerX = drawX;
         this.centerY = drawY;
         this.polygonR = polygonR;
@@ -55,7 +55,7 @@ public abstract class BaseBubble {
         this.mHeight = height;
         this.paint = new Paint(Paint.ANTI_ALIAS_FLAG);  
         this.paint.setStyle(Paint.Style.STROKE);
-       this.paint.setColor(Color.argb(255, (int) (Math.random() * 255), (int) (Math.random() * 255), (int) (Math.random() * 255)));
+        this.paint.setColor(Color.argb(255, (int) (Math.random() * 255), (int) (Math.random() * 255), (int) (Math.random() * 255)));
         this.paint.setStrokeWidth(4);
         this.paint.setTextSize(64);
         this.paint.setAntiAlias(true);
@@ -63,9 +63,9 @@ public abstract class BaseBubble {
    /**
      * change Hexagon and circle's colors
      */
-      public void changeColor() {
+  public void changeColor() {
         this.paint.setColor(Color.argb(255, (int) (Math.random() * 255), (int) (Math.random() * 255), (int) (Math.random() * 255)));
-    }
+  }
    /**
      * create a abstract class to make the subclass implementation
      */
@@ -75,7 +75,7 @@ public abstract class BaseBubble {
     //  @param x
      // @param y
      
-      public void move(float x, float y) {
+  public void move(float x, float y) {
         centerX = x;
         centerY = y;
         //Determine whether the center of  horizontal and vertical axis are acceptable or not
@@ -95,8 +95,24 @@ public abstract class BaseBubble {
             mDy = -mDy;
         }
     }
-  
-  
+    
+    /**
+     * Check if it out of view's bounds
+     */
+    public boolean isCross(float x, float y) {
+        if (x < polygonR) {
+            return true;
+        } else if (x > mWidth) {
+            return true;
+        }
+        //check y coordinate below
+        if (y < polygonR) {
+            return true;
+        } else if (y > mHeight) {
+            return true;
+        }
+        return false;
+    }
   
 
 }
