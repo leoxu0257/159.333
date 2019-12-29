a6890f4898e83315230c0a992d32bdf83deec27e
ArnoxN1 <43973107+ArnoxN1@users.noreply.github.com>
2019-12-29 14:15:25 +1300

    Update CircularBubble

--- app/src/main/java/com/example/martin/bubbles/CircularBubble.java
+++ app/src/main/java/com/example/martin/bubbles/CircularBubble.java
@@ -3,5 +3,35 @@ package com.example.martin.bubbles;
 import android.graphics.Canvas;
 
 public class CircularBubble extends BaseBubble {
+  
+  public CircularBubble(float drawX, float drawY, float polygonR, int width, int height) {
+        super(drawX, drawY, polygonR, width, height);
+    }
 
+    @Override
+    public void onDraw(Canvas canvas) {
+        canvas.save();// store canvas states
+        canvas.translate(centerX, centerY);// Canvas move to the starting position of the bubble
+        canvas.drawCircle(0, 0, polygonR, paint);// draw circle
+        canvas.restore();// restore last save states
+    }
+
+    /**
+     * Check if is inside
+     */
+    @Override
+    public boolean isInside(float x, float y) {
+        return ((x - centerX) * (x - centerX) + (y - centerY) * (y - centerY)) < (polygonR * polygonR);
+    }
+
+    @Override
+    public boolean isCollision(BaseBubble bubble) {
+        return getCenterDistance(bubble) <= polygonR + bubble.polygonR;
+    }
+
+    @Override
+    public float getIntersectDistance(BaseBubble bubble) {
+        return polygonR + bubble.polygonR - getCenterDistance(bubble);
+    }
+  
 }
