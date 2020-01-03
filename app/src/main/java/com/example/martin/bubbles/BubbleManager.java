43bd8b8308fb5fe3fafbdf6bf6d76664939c2980
ArnoxN1 <43973107+ArnoxN1@users.noreply.github.com>
2020-01-03 15:10:42 +1300

    Update BubbleManager.java

--- app/src/main/java/com/example/martin/bubbles/BubbleManager.java
+++ app/src/main/java/com/example/martin/bubbles/BubbleManager.java
@@ -31,6 +31,7 @@ public class BubbleManager {
 
     private int mHeight;
 
+    // Create random number
     private Random mRandom;
 
     private BubbleType mBubbleType;
@@ -65,6 +66,8 @@ public class BubbleManager {
             int startY = ceilSize / 2 + (i / column) * ceilSize;
 
             BaseBubble bubble = null;
+            
+            // deal with factory
             switch (bubbleType) {
                 case TRIANGLE:
                     bubble = new TriangleBubble(startX, startY, polygonR, width, height);
@@ -157,5 +160,76 @@ public class BubbleManager {
         if (dt != 0) fps = 1000000000.0f / dt;
         canvas.drawText(String.format("FPS:%.2s", fps), 16, 192, mTextPaint);
         mLastTime = time;
+    }
+    
+    /**
+     * update bubble
+     */
+    public void update(float dt, boolean isCollide) {
+        for (BaseBubble bubble : mBubbleList) {
+            bubble.move(dt);
+        }
+        if (isCollide) {
+            collide();
+        }
+    }
+
+    /**
+     * Collision speed loss coefficient 
+     */
+    float mDampening = 0.0f;
+
+    /**
+     * Collision detection
+     */
+    private void collide() {
+        List<BaseBubble> temp = new ArrayList<>();// list of bubbles
+        for (BaseBubble bubble : mBubbleList) {
+            if (bubble.isMove) {
+                temp.add(bubble);
+                continue;
+            }
+            handleCollide(bubble);// Check if a bubble collides with other bubbles
+        }
+        for(BaseBubble bubble:temp){
+            mBubbleList.remove(bubble);// remove bubble from the list (delete bubbles) 
+        }
+    }
+    
+    /**
+     * Calculate velocity after collision without energy loss based on conservation of energy and impulse
+     */
+    private float[] getSpeed(float m1, float m2, float v1, float v2) {
+        float totalMv = m1 * v1 + m2 * v2;// Impulse before collision
+        float totalMv2 = (m1 * v1 * v1 + m2 * v2 * v2);// did not multipled by 1/2
+        float v[] = new float[2];// Two speeds after collision
+
+        // Elimination below
+        float a = m1 * m2 + m1 * m1;
+        float b = -2 * totalMv * m1;
+        float c = totalMv * totalMv - totalMv2 * m2;
+        float d = (b * b - 4 * a * c);
+        if (a == 0) {
+            v[0] = -c / b;
+            v[1] = -c / b;
+        } else if (d >= 0) {// have root
+            float tempV1 = (float) (-b + Math.sqrt(d)) / (2 * a);
+            float tempV2 = (float) (-b - Math.sqrt(d)) / (2 * a);
+            if (Math.abs(tempV1 - v1) <= 0.001) {// There are two roots in the calculation. If one root is the same as before, don't use this root.
+                v[0] = tempV2;
+            } else {
+                v[0] = tempV1;
+            }
+            v[1] = (totalMv - m1 * v[0]) / m2;
+        }
+        v[0] = (1 - mDampening) * v[0];// Multiplied by a collision velocity loss coefficient
+        v[1] = (1 - mDampening) * v[1];
+        if (v[0] == 0) {
+            v[0] = 1;
+        }
+        if (v[1] == 0) {
+            v[1] = 1;
+        }
+        return v;
     }
 }
